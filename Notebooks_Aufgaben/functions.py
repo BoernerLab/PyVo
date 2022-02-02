44d93978f3278bcc904656fc7d968d9206e3f45a
Lreuss <92789727+Lreuss@users.noreply.github.com>
2022-02-02 15:05:59 +0100

    Deleted function.py

--- Notebooks_Aufgaben/functions.py
+++
@@ -1,8 +0,0 @@
-def create_step_vector(start, end, stepwidth = 1):
-    step_vector = [start]
-    number_of_digits_after_decimal_point = len(str(stepwidth-int(stepwidth))[2:])
-    i = start
-    while i < end:
-        i = round(i + stepwidth,  number_of_digits_after_decimal_point)
-        step_vector.append(i)
-    return step_vector 
